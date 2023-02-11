d6ebc1c688c861681517e4ee7942dfaabd89f696
macow-lab <mmacovv@gmail.com>
2023-02-11 14:38:08 +0100

    brrr

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"github.com/darth-raijin/borealis/api/handlers"
+	"github.com/darth-raijin/borealis/api/controllers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -32,17 +32,17 @@ func Initialize() *fiber.App {
 
 	// Auth endpoints
 	auth := v1.Group("/auth")
-	auth.Get("/login", handlers.GetEventById)
-	auth.Post("/login", handlers.GetEventById)
+	auth.Get("/login", controllers.GetEventById)
+	auth.Post("/login", controllers.GetEventById)
 
-	auth.Get("/register", handlers.GetEventById)
-	auth.Post("/register", handlers.GetEventById)
+	auth.Get("/register", controllers.GetEventById)
+	auth.Post("/register", controllers.GetEventById)
 
 	// Profile
 	profile := v1.Group("/profile")
-	profile.Get("/profile", handlers.GetEventById)
-	profile.Put("/profile", handlers.GetEventById)
-	profile.Delete("/profile", handlers.GetEventById)
+	profile.Get("/profile", controllers.GetEventById)
+	profile.Put("/profile", controllers.GetEventById)
+	profile.Delete("/profile", controllers.GetEventById)
 
 	return app
 }
