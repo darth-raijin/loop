79c08de23e353db79edc8de615b856fb60a3ea5e
w31708 <mohamed.macow@ufst.dk>
2023-03-07 13:32:15 +0100

    Initial work on DB

--- backend/pkg/repository/database.go
+++ backend/pkg/repository/database.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"database/sql"
+	"log"
+)
+
+var db *sql.DB
+
+func ConnectDatabase() {
+	connection := "postgresql://<username>:<password>@<database_ip>/todos?sslmode=disable"
+	// Connect to database
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func validateConnection(*sql.DB) {
+
+}
