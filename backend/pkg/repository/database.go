8c92309a0d66288fc20f43ba49a973189b550c01
w31708 <mohamed.macow@ufst.dk>
2023-03-08 13:05:57 +0100

    Database connection config to be read from .env

--- backend/pkg/repository/database.go
+++ backend/pkg/repository/database.go
@@ -2,13 +2,26 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 )
 
 var db *sql.DB
 
 func ConnectDatabase() {
-	connection := "postgresql://<username>:<password>@<database_ip>/todos?sslmode=disable"
+	// Values set from .env
+	username := "postgres"
+	password := "postgres"
+	host := "localhost"
+	port := 5432
+	database := "tolder"
+	connection := fmt.Sprintf("postgresql://%v:%v@%v:%v/%v?sslmode=disable",
+		username,
+		password,
+		host,
+		port,
+		database,
+	)
 	// Connect to database
 	db, err := sql.Open("postgres", connection)
 	if err != nil {
