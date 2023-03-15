657b35afc6de0b37b5e7ab786f9d65f243d68f68
macow-lab <mmacovv@gmail.com>
2023-03-15 19:32:35 +0100

    Fix INCORRECT struct tags

--- backend/api/models/dtos/event/create/createEventResponse.go
+++ backend/api/models/dtos/event/create/createEventResponse.go
@@ -3,8 +3,8 @@ package createEventDto
 import "github.com/google/uuid"
 
 type CreateEventResponse struct {
-	ID          uuid.UUID `json:"id""`
-	Name        string    `json:"name""`
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
 	Description string    `json:"description"`
 	City        string    `json:"city,omitempty"`
 	Country     string    `json:"country,omitempty"`
